2e50ea21dbe842038f2328dd0a0028e469662795
IdenTiclla <iden.ticlla@gmail.com>
2021-07-28 11:57:43 -0400

    add Post model

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
+from typing import Text
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Text, Optional
+from datetime import date, datetime
+
 
 app = FastAPI()
 
 posts = [
     
 ]
+
+# Post MOdel
+
+class Post(BaseModel):
+    id: Optional[str]
+    title: str
+    author: str
+    content: Text
+    created_at: datetime = datetime.now()
+    published_at: Optional[datetime]
+    published: bool = False
+
 
 @app.get('/')
 def read_root():
@@ -15,3 +32,8 @@ def read_root():
 @app.get('/posts')
 def get_posts():
     return posts
+
+@app.post('/posts')
+def save_post(post: Post):
+    print(post.dict())
+    return "received"
