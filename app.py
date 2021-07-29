7d7dd5659f5d8c64428b83685c27fcff05010093
IdenTiclla <iden.ticlla@gmail.com>
2021-07-29 13:54:42 -0400

    add delte specific post

--- app.py
+++ app.py
@@ -43,6 +43,17 @@ def get_post(post_id: str):
 
     raise HTTPException(status_code=404, detail="Post Not Found.")
 
+@app.delete('/posts/{post_id}')
+def delete_post(post_id: str):
+    for index, post in enumerate(posts):
+        if post["id"] == post_id:
+            posts.pop(index)
+
+            return {"message": "Post has been deleted successfully"}
+
+    raise HTTPException(status_code=404, detail="Post Not Found.")
+
+
 @app.post('/posts')
 def save_post(post: Post):
     post.id = str(uuid())
